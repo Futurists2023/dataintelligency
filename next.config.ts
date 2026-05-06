6f0ec2d92d4bf4cb02900ce3585520ef5cd2c8bc
MankweAI <kgoshimankwe@outlook.com>
2026-05-06 20:33:44 +0200

    New logo added

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/logo.svg",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "image/svg+xml",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
